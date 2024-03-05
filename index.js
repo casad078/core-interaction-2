22045f831f328688282eeefe3158d5f6cf0b1d00
Dani C <143529026+casad078@users.noreply.github.com>
2024-03-04 21:58:51 -0500

    fixing gif

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var originalBackgroundColor = document.body.style.backgroundColor;
 document.getElementById('colorButton').addEventListener('click', function() {
   var body = document.body;
   if (body.style.backgroundColor === 'black') {
-    // If background color is black, restore original background color
+   
     body.style.backgroundColor = originalBackgroundColor;
     this.textContent = 'lights off';
   } else {
-    // Change background color to black
+
     body.style.backgroundColor = 'black';
     this.textContent = 'lights on';
   }
@@ -21,12 +21,12 @@ const generateButton = document.getElementById('generateButton');
 const maxStars = 200;
 let appearanceSpeed = 80; // Default appearance speed in milliseconds
 
-// Function to generate a random number within a range
+
 function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// Function to generate a star
+
 function createStar() {
   const star = document.createElement('span');
   star.classList.add('star');
@@ -36,7 +36,7 @@ function createStar() {
   container.appendChild(star);
 }
 
-// Function to generate stars one by one with a delay
+
 function generateStars() {
   if (container.querySelectorAll('.star').length < maxStars) {
     createStar();
@@ -46,5 +46,5 @@ function generateStars() {
   }
 }
 
-// Event listener for the button click
+
 generateButton.addEventListener('click', generateStars);

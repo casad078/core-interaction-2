e4f4867af065d5039a13a9401fd0fe1d24d46e19
Dani C <143529026+casad078@users.noreply.github.com>
2024-03-27 23:24:28 -0400

    p2

---
+++ p2/home/script.js
@@ -0,0 +1,8 @@
+
+  var hideButton = document.getElementById('hideButton');
+  var container = document.getElementById('blur');
+
+  hideButton.addEventListener('click', function() {
+    container.style.display = 'none';
+    hideButton.style.display = 'none';
+  });

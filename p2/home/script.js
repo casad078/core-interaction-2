97ba5470c6cc64eb7b77a9c0764dd45e2348db16
Dani C <143529026+casad078@users.noreply.github.com>
2024-04-04 16:57:17 -0400

    for now

--- p2/home/script.js
+++ p2/home/script.js
@@ -2,3 +2,351 @@ window.addEventListener('load', function() {
     var loadDiv = document.getElementById('load');
     loadDiv.style.display = 'none';
   });
+
+
+//images//
+const images =
+    [
+    { 
+        src: 'collections_images/img1.png', 
+        link: 'https://www.coolsymbol.top/',
+        'postcard': false,
+        'photograph':false,
+        'cards': true,
+        'prints': false,
+        'collaged': false,
+        'stickers': false,
+        'messages': false
+    },
+    { 
+        src: 'collections_images/img2.png', 
+        link: 'https://www.coolsymbol.top/',
+        'postcard': false,
+        'photograph':false,
+        'cards': true,
+        'prints': false,
+        'collaged': false,
+        'stickers': false,
+        'messages': false
+    },
+    {   
+        src: 'collections_images/img3.png', 
+        link: 'https://www.coolsymbol.top/',
+        'postcard': false,
+        'photograph':false,
+        'cards': true,
+        'prints': false,
+        'collaged': false,
+        'stickers': false,
+        'messages': false
+    },
+    {   
+        src: 'collections_images/img4.png', 
+        'postcard': false,
+        'photograph':false,
+        'cards': true,
+        'prints': false,
+        'collaged': false,
+        'stickers': false,
+        'messages': false
+    },
+    {   
+        src: 'collections_images/img5.png', 
+        'postcard': false,
+        'photograph':false,
+        'cards': true,
+        'prints': false,
+        'collaged': false,
+        'stickers': false,
+        'messages': false
+    },
+    {   
+        src: 'collections_images/img6.png', 
+        'postcard': false,
+        'photograph':false,
+        'cards': true,
+        'prints': false,
+        'collaged': false,
+        'stickers': false,
+        'messages': false
+    },
+    {   
+        src: 'collections_images/img7.png', 
+        'postcard': false,
+        'photograph':false,
+        'cards': true,
+        'prints': false,
+        'collaged': false,
+        'stickers': false,
+        'messages': false
+    },
+    {   
+        src: 'collections_images/img8.png', 
+        'postcard': false,
+        'photograph':false,
+        'cards': true,
+        'prints': false,
+        'collaged': false,
+        'stickers': false,
+        'messages': false
+    },
+    {   
+        src: 'collections_images/img9.png', 
+        'postcard': false,
+        'photograph':false,
+        'cards': true,
+        'prints': false,
+        'collaged': false,
+        'stickers': false,
+        'messages': false
+    },
+    {   
+        src: 'collections_images/img10.png', 
+        'postcard': false,
+        'photograph':false,
+        'cards': true,
+        'prints': false,
+        'collaged': false,
+        'stickers': false,
+        'messages': false
+    },
+    {   
+        src: 'collections_images/img11.png', 
+        'postcard': false,
+        'photograph':false,
+        'cards': true,
+        'prints': false,
+        'collaged': false,
+        'stickers': false,
+        'messages': false
+    },
+    {   
+        src: 'collections_images/img12.png', 
+        'postcard': false,
+        'photograph':true,
+        'cards': false,
+        'prints': false,
+        'collaged': false,
+        'stickers': false,
+        'messages': false
+    },
+    {   
+        src: 'collections_images/img13.png', 
+        'postcard': false,
+        'photograph':false,
+        'cards': true,
+        'prints': false,
+        'collaged': false,
+        'stickers': false,
+        'messages': false
+    },
+    {   
+        src: 'collections_images/img14.png', 
+        'postcard': true,
+        'photograph':false,
+        'cards': false,
+        'prints': true,
+        'collaged': false,
+        'stickers': false,
+        'messages': false
+    },
+    {   
+        src: 'collections_images/img15.png', 
+        'postcard': true,
+        'photograph':false,
+        'cards': false,
+        'prints': false,
+        'collaged': false,
+        'stickers': false,
+        'messages': false
+    },
+    {   
+        src: 'collections_images/img16.png', 
+        'postcard': true,
+        'photograph':false,
+        'cards': false,
+        'prints': false,
+        'collaged': false,
+        'stickers': false,
+        'messages': false
+    },
+    {   
+        src: 'collections_images/img17.png',
+        'postcard': true,
+        'photograph':false,
+        'cards': false,
+        'prints': false,
+        'collaged': false,
+        'stickers': false,
+        'messages': false 
+    },
+    {   
+        src: 'collections_images/img18.png', 
+        'postcard': false,
+        'photograph':false,
+        'cards': false,
+        'prints': false,
+        'collaged': true,
+        'stickers': false,
+        'messages': false
+    },
+    {   
+        src: 'collections_images/img19.png', 
+        'postcard': false,
+        'photograph':false,
+        'cards': false,
+        'prints': false,
+        'collaged': true,
+        'stickers': false,
+        'messages': false
+    },
+    {   
+        src: 'collections_images/img20.png', 
+        'postcard': false,
+        'photograph':false,
+        'cards': true,
+        'prints': false,
+        'collaged': false,
+        'stickers': false,
+        'messages': false
+    },
+    {   
+        src: 'collections_images/img21.png', 
+        'postcard': false,
+        'photograph':false,
+        'cards': false,
+        'prints': false,
+        'collaged': false,
+        'stickers': true,
+        'messages': false
+    },
+    {   
+        src: 'collections_images/img22.png', 
+        'postcard': false,
+        'photograph':false,
+        'cards': false,
+        'prints': false,
+        'collaged': true,
+        'stickers': false,
+        'messages': false
+    },
+    {   
+        src: 'collections_images/img23.png', 
+    },
+    {   
+        src: 'collections_images/img24.png', 
+    },
+    {   
+        src: 'collections_images/img25.png', 
+    },
+    {   
+        src: 'collections_images/img26.png', 
+    },
+    {   
+        src: 'collections_images/img27.png', 
+    },
+    {   
+        src: 'collections_images/img28.png', 
+    },
+    {   
+        src: 'collections_images/img29.png', 
+    },
+    {   
+        src: 'collections_images/img30.png', 
+    },
+    {   
+        src: 'collections_images/img31.png', 
+    },
+    {   
+        src: 'collections_images/img32.png', 
+    },
+    {   
+        src: 'collections_images/img33.png', 
+    },
+    {   
+        src: 'collections_images/img34.png', 
+    },
+    {   
+        src: 'collections_images/img35.png', 
+    },
+    {   
+        src: 'collections_images/img36.png', 
+    },
+    {   
+        src: 'collections_images/img37.png', 
+    },
+    {   
+        src: 'collections_images/img38.png', 
+    },
+    {   
+        src: 'collections_images/img39.png', 
+    },
+    {   
+        src: 'collections_images/img40.png', 
+    },
+    {   
+        src: 'collections_images/img41.png', 
+    },
+    {   
+        src: 'collections_images/img42.png', 
+    },
+    {   
+        src: 'collections_images/img43.png', 
+    },
+    {   
+        src: 'collections_images/img44.png', 
+    },
+    {   
+        src: 'collections_images/img45.png', 
+    },
+    {   
+        src: 'collections_images/img46.png', 
+    },
+    {   
+        src: 'collections_images/img47.png', 
+    },
+    {   
+        src: 'collections_images/img48.png', 
+    },
+    {   
+        src: 'collections_images/img49.png', 
+    },
+    {   
+        src: 'collections_images/img50.png', 
+    },
+    {   
+        src: 'collections_images/img51.png', 
+    },
+    {   
+        src: 'collections_images/img52.png', 
+    },
+    {   
+        src: 'collections_images/img53.png', 
+    },
+    {   
+        src: 'collections_images/img54.png', 
+    },
+    {   
+        src: 'collections_images/img55.png', 
+    },
+    {   
+        src: 'collections_images/img56.png', 
+    },
+    ];
+
+    function displayImages(images) {
+        const container = document.getElementById('masonry');
+        container.innerHTML = ''; // Clear previous images
+      
+        images.forEach(image => {
+                const imgElement = document.createElement('a');
+                imgElement.href = image.link;
+                const img = document.createElement('img');
+                img.src = image.src;
+                imgElement.appendChild(img);
+                container.appendChild(imgElement);
+        });
+      }
+
+      // Initial display of all images
+      displayImages(images);
